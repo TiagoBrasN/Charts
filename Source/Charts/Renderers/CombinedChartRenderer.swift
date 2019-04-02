c8f724605be7d69e3c742a5cf9201c9c0d308368
Bob De Kort <bogo@nodes.dk>
2019-04-02 14:20:29 +0200

    Change createRenderers access level from internal to public, That way we can change the line renderer for the combinedChartRenderer

--- Source/Charts/Renderers/CombinedChartRenderer.swift
+++ Source/Charts/Renderers/CombinedChartRenderer.swift
@@ -36,7 +36,7 @@ open class CombinedChartRenderer: DataRenderer
     }
     
     /// Creates the renderers needed for this combined-renderer in the required order. Also takes the DrawOrder into consideration.
-    internal func createRenderers()
+    public func createRenderers()
     {
         _renderers = [DataRenderer]()
         
